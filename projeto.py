e0dfba51873ffabeb9b8fcca4fffd15d8260affd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:02:55 -0300

    TOMA LARI AQUI

---
+++ projeto.py
@@ -0,0 +1,53 @@
+#Divisão de tarefas 
+eventos = {
+    "Rock in Rio": {
+            "Data":"12/12/2023",
+            "Capacidade":"10,000",
+            "Localização":"Neo Química Arena"
+    },
+    "Lolapalooza": {
+            "Data":"15/01/2024",
+            "Capacidade":"15,000",
+            "Localização": "Maracanã"
+    },
+
+}
+
+def menu(): #Menu inicial de input, vai receber um int 
+             #para selecionar um dos serviços que vamos ter
+    pass
+
+def criar_evento (): """Criar_Evento, permite o usuário a ctriar um novo evento, 
+                      fornecendo, nome,data, capacidade e localização 
+                      temos que criar um dicionário pra cada evento."""
+
+    pass
+
+def  listar_eventos ():
+        """reservar_vaga permite o usuário reservar uma vaga em um evento em especifico""" 
+        pass
+    
+        """Exibe uma lista de todos os eventos 
+                           dísponives com todas as informações básicas
+                            Apenas exibir."""          
+
+def reservar_vaga (): 
+    return pass
+                         """reservar_vaga permite o usuário reservar uma vaga em um evento em especifico"""
+                        
+def cancelar_reserva (): 
+    return pass
+                  """Permite o usuário remover uma reserva"""
+def visualizar_detalhes_evento ():
+    return pass
+                           """Exibe detalhes de um evento"""
+def salvar_dados ():
+    return pass
+                            """salvar dados, salva todos os dados de um sistema de um arquivo"""
+def carregar_dados ():
+    return pass
+
+
+                             """carrega os dados do sistema a partir de um arquivo"""
+if __name__ == "__main__":
+    menu()
