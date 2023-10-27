e90678a0de9bc72f8e31f9d74598dfc927761673
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-27 10:47:39 -0300

    arquivo inicial com comentários

--- projeto.py
+++ projeto.py
@@ -13,41 +13,53 @@ eventos = {
 
 }
 
-def menu(): #Menu inicial de input, vai receber um int 
-             #para selecionar um dos serviços que vamos ter
+def menu(): 
+    """Menu inicial de input, vai receber um int para selecionar 
+    um dos serviços que vamos ter"""
     pass
 
-def criar_evento (): """Criar_Evento, permite o usuário a ctriar um novo evento, 
-                      fornecendo, nome,data, capacidade e localização 
-                      temos que criar um dicionário pra cada evento."""
+def criar_evento (): 
+    """Criar_Evento, permite o usuário a criar um novo evento,
+    fornecendo, nome,data, capacidade e localização 
+    temos que criar um dicionário pra cada evento e armazenar no sistema. """
 
     pass
 
 def  listar_eventos ():
-        """reservar_vaga permite o usuário reservar uma vaga em um evento em especifico""" 
-        pass
+    """Exibe uma lista de todos os eventos dísponives 
+    com todas as informações básicas.Apenas exibir lista de 
+    eventos formatada com detalhes."""    
+    pass
     
-        """Exibe uma lista de todos os eventos 
-                           dísponives com todas as informações básicas
-                            Apenas exibir."""          
+             
 
 def reservar_vaga (): 
-    return pass
-                         """reservar_vaga permite o usuário reservar uma vaga em um evento em especifico"""
+    """reservar_vaga permite o usuário reservar uma vaga em um 
+    evento em especifico, é necessário dados do evento e confirmação
+    da reserva"""
+    pass
+                         
                         
 def cancelar_reserva (): 
-    return pass
-                  """Permite o usuário remover uma reserva"""
+    """Permite o usuário remover uma reserva. Necessário dados 
+     do evento e confirmação do cancelamento"""
+    pass
+                 
+    
+
 def visualizar_detalhes_evento ():
-    return pass
-                           """Exibe detalhes de um evento"""
+    """Exibe detalhes de um evento. Dados que deverá exibir: nome,
+      data, localização, capacidade e vagas disponíveis. Retorna esses detalhes"""
+    pass
+                           
 def salvar_dados ():
-    return pass
-                            """salvar dados, salva todos os dados de um sistema de um arquivo"""
+    """salva todos os dados do sistema (eventos, reservas e etc.) de um arquivo """
+    pass
+                            
 def carregar_dados ():
-    return pass
+    """carrega os dados do sistema a partir de um arquivo. Deve confirmar que os dados foram carregados, incluir erros para arquivo inexistente ou corrompido"""
+    pass
 
-
-                             """carrega os dados do sistema a partir de um arquivo"""
+                             
 if __name__ == "__main__":
     menu()
